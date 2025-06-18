c0bbe137bfc7b6b292f8d8e339b93aea92fa1c79
kaudotdev <kauansouzaesousa@gmail.com>
2025-06-17 23:07:20 -0300

    :tada: Initial Commit

---
+++ src/main/java/org/example/minevidencia/essentialEmEvidencia/EssentialEmEvidencia.java
@@ -0,0 +1,14 @@
+package org.example.minevidencia.essentialEmEvidencia;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EssentialEmEvidencia extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
